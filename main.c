96591d324713f10a1446e40a67c7869831feb612
dalibelhadj <mohamedali.belhadj@esprit.tn>
2021-10-18 21:17:06 +0100

    ajout

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+int a;
+int b;
+printf("\n saisie du premier nombre : ");
+scanf("%d",&a);
+do{
+printf("\n saisie du second nombre : ");
+scanf("%d",&b);
+}while(b==0);
+printf("\n%d+%d=%d",a,b,a+b);
+printf("\n%d-%d=%d",a,b,a-b);
+printf("\n%d/%d=%f\n",a,b,(float)a/b);
+}
+return(0);
